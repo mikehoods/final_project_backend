88dd39b44a107d81a02fab7de4cf52a460c1eea5
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-06-27 12:39:02 -0500

    got POST working on FE.

--- api/resources.py
+++ api/resources.py
@@ -1,7 +1,9 @@
 from tastypie.resources import  ModelResource
 from api.models import Entry
+from tastypie.authorization import Authorization
 
 class EntryResource(ModelResource):
     class Meta:
         queryset = Entry.objects.all()
         resource_name = 'entry'
+        authorization = Authorization()
